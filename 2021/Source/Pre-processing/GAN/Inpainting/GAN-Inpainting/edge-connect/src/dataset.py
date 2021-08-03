6ec37ad5c362ae294a57ac5d9e0af55e487b0656
tansyab1 <tansyab1@gmail.com>
2021-08-03 10:25:54 +0200

    Enter

--- 2021/Source/Pre-processing/GAN/Inpainting/GAN-Inpainting/edge-connect/src/dataset.py
+++ 2021/Source/Pre-processing/GAN/Inpainting/GAN-Inpainting/edge-connect/src/dataset.py
@@ -165,7 +165,7 @@ class Dataset(torch.utils.data.Dataset):
             i = (imgw - side) // 2
             img = img[j:j + side, i:i + side, ...]
 
-        img = scipy.misc.imresize(img, [height, width])
+        img = np.array(Image.fromarray(img).resize([height, width]))
 
         return img
 
