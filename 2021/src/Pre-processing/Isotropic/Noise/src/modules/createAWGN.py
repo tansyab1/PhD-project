f68d3ef3dd0a6c53fdfcb3d50a7e10ff80b94cd5
tansyab1 <tansyab1@gmail.com>
2021-08-17 10:17:14 +0200

    redir

--- 2021/src/Pre-processing/Isotropic/Noise/src/modules/createAWGN.py
+++ 2021/src/Pre-processing/Isotropic/Noise/src/modules/createAWGN.py
@@ -3,20 +3,24 @@ import cv2
 import numpy as np
 from skimage.util import random_noise
 import os
-def create_noise(image,var=0.01,mean=0):
-	noise_img = random_noise(img, mode='gaussian',mean=mean, var=var,clip=True)
-	return np.array(255*noise_img, dtype = 'uint8')
+
+
+def create_noise(image, var=0.01, mean=0):
+    noise_img = random_noise(img, mode='gaussian',
+                             mean=mean, var=var, clip=True)
+    return np.array(255*noise_img, dtype='uint8')
+
 
 if __name__ == "__main__":
     save_path = '/home/nguyentansy/PhD-work/PhD-project/2021/Source/Pre-processing/Isotropic/results/AWGN/'
     os.makedirs(save_path, exist_ok=True)
-    sigmas=[0.0005,0.001,0.005,0.01]
+    sigmas = [0.0005, 0.001, 0.005, 0.01]
     img = cv2.imread('results/test.jpg')
     for sigma in sigmas:
         # cv2.imshow('Original',img)
-        file_name ='AWGN'+'_var:'+str(sigma)+'.png'
+        file_name = 'AWGN'+'_var:'+str(sigma)+'.png'
         completeName = os.path.join(save_path, file_name)
-        output = create_noise(img,var=sigma)
+        output = create_noise(img, var=sigma)
         # cv2.imshow('Motion Blur', output)
-        cv2.imwrite(completeName,output)
+        cv2.imwrite(completeName, output)
         # cv2.waitKey(0)
