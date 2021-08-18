586660162080147b338f79a44fff1ef87c3c1136
tansyab1 <tansyab1@gmail.com>
2021-08-18 11:33:10 +0200

    check Blur

--- 2021/src/Pre-processing/Isotropic/Blur/src/modules/checkBlur.py
+++ 2021/src/Pre-processing/Isotropic/Blur/src/modules/checkBlur.py
@@ -1,4 +1,6 @@
 import cv2
+import os
+import matplotlib.pyplot as plt
 
 
 def detectBlur(img_path, threhold):
@@ -11,9 +13,28 @@ def detectBlur(img_path, threhold):
         return False
 
 
+def readImagefromFolderpath(folder_path):
+    img_list = []
+    for file in os.listdir(folder_path):
+        if file.endswith(".jpg"):
+            img_list.append(file)
+    return img_list
+
+
+def plotHistoram(arr):
+    plt.hist(arr, bins=256)
+    plt.show()
+
+
 def getVarianofLaplace(img):
     return cv2.Laplacian(img, cv2.CV_64F).var()
 
 
 if __name__ == '__main__':
     print(detectBlur("../../../../Downloads/blur.jpg", 0.01))
+    var_list = []
+    img_list = readImagefromFolderpath("../../../../Downloads/blur")
+    for img in img_list:
+        var_list.append(getVarianofLaplace(
+            cv2.imread("../../../../Downloads/blur/" + img)))
+    plotHistoram(var_list)
