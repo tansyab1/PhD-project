5d3c30c3c1165c6e67412bcb522b2e94158706ca
tansyab1 <tansyab1@gmail.com>
2021-08-18 11:37:02 +0200

    comment

--- 2021/src/Pre-processing/Isotropic/Blur/src/modules/checkBlur.py
+++ 2021/src/Pre-processing/Isotropic/Blur/src/modules/checkBlur.py
@@ -4,6 +4,15 @@ import matplotlib.pyplot as plt
 
 
 def detectBlur(img_path, threhold):
+    """detect blur of the image
+
+    Args:
+        img_path (string): image path
+        threhold (float): threshold of blur
+
+    Returns:
+        Bool: yes or no
+    """
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     var = getVarianofLaplace(gray)
@@ -14,6 +23,12 @@ def detectBlur(img_path, threhold):
 
 
 def readImagefromFolderpath(folder_path):
+    """read all images from folder
+    Args:
+        folder_path (string): folder
+    Returns:
+        list: list of image path
+    """
     img_list = []
     for file in os.listdir(folder_path):
         if file.endswith(".jpg"):
@@ -22,11 +37,23 @@ def readImagefromFolderpath(folder_path):
 
 
 def plotHistoram(arr):
+    """plot histogram of the array
+    Args:
+        arr (list): array
+    Returns:
+        None
+    """
     plt.hist(arr, bins=256)
     plt.show()
 
 
 def getVarianofLaplace(img):
+    """get variance of laplace of the image
+    Args:
+        img (numpy array): image
+    Returns:
+        float: variance of laplace
+    """
     return cv2.Laplacian(img, cv2.CV_64F).var()
 
 
