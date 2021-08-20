074a536d2d26d12728aedac9370dc497e880bc8f
tansyab1 <tansyab1@gmail.com>
2021-08-20 17:09:39 +0200

    update

--- 2021/src/Classification/Pre-Trained-DenseNet-161/Dataloader_with_path.py
+++ 2021/src/Classification/Pre-Trained-DenseNet-161/Dataloader_with_path.py
@@ -3,10 +3,6 @@ from torchvision import datasets
 # from dataset.Dataloader_with_path import ImageFolderWithPaths
 
 
-
-
-
-
 class ImageFolderWithPaths(datasets.ImageFolder):
     """Custom dataset that includes image file paths. Extends
     torchvision.datasets.ImageFolder
@@ -14,7 +10,7 @@ class ImageFolderWithPaths(datasets.ImageFolder):
 
     # override the __getitem__ method. this is the method that dataloader calls
     def __getitem__(self, index):
-        # this is what ImageFolder normally returns 
+        # this is what ImageFolder normally returns
         original_tuple = super(ImageFolderWithPaths, self).__getitem__(index)
         # the image file path
         path = self.imgs[index][0]
@@ -23,7 +19,7 @@ class ImageFolderWithPaths(datasets.ImageFolder):
         return tuple_with_path
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
     root_path = "/work/vajira/data/kvasir_new_23_class/data/split_0"
 
