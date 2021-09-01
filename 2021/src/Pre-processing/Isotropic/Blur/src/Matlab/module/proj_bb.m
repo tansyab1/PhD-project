a004c62612581f62bb5f9d2307d65fc5bdb5cebf
tansyab1 <tansyab1@gmail.com>
2021-09-01 15:56:11 +0200

    check

--- 2021/src/Pre-processing/Isotropic/Blur/src/Matlab/module/proj_bb.m
+++
@@ -1,3 +0,0 @@
-function x = proj_bb(u,lambda)
-x = min(max(u,-ones(size(u))*lambda),ones(size(u))*lambda);
-end
--- 2021/src/Pre-processing/Isotropic/Blur/src/Matlab/module/proj_u.m
+++
@@ -1,5 +0,0 @@
-function u_pr = proj_u(u, lambda)
-
-u_pr = (-lambda<=u & u<=lambda).*u + (u>lambda).*lambda + (u<-lambda).*-lambda;
-
-end
