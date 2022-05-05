b554653e48f453eb31ea649287bc66d818e204fc
tansyab1 <tansyab1@gmail.com>
2022-05-05 11:46:26 +0200

    update

--- copy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scp -r /home/nguyentansy/DATA/PhD-work/PhD-project/2021/src/Classification/kvasir-capsule/ tansy.nguyen@lipnssh.univ-paris13.fr:~/src/ 
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-srun python3 src/kvasir-capsule/classification_experiments/Modified-ResNet-152/fine-tuned-kvasircapsule-clean.py --best_resnet dataport/output/ref/train-0_val-1/fine-tuned-kvasircapsule.py/checkpoints/fine-tuned-kvasircapsule.py_epoch\:48.pt --num_epochs 50 train --device_id 0
