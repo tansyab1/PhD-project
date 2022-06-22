25b4d509752d41358deeafc39ea0e8f948f65882
tansyab1 <tansyab1@gmail.com>
2022-06-22 13:02:49 +0200

    update

--- 2021/src/Pre-processing/Isotropic/UnevenIllumination/src/jnc.py
+++ 2021/src/Pre-processing/Isotropic/UnevenIllumination/src/jnc.py
@@ -78,15 +78,15 @@ def gabor_filter(img, filters):
     
     gabor_img = np.zeros(img.shape)
     for kern in filters:
-        
-        gabor_img += cv2.filter2D(img, cv2.CV_64F, kern, borderType=cv2.BORDER_REFLECT)
+        gabor_img += np.abs(cv2.filter2D(img, cv2.CV_64F, kern, borderType=cv2.BORDER_REFLECT))
     return gabor_img/4
 
 def jnc_plus(img, filter=None, name=None):
+    plt.figure()
     illmask = cv2.medianBlur(img, 201)
-    gabor_img=gabor_filter(img, filter)
+    gabor_img=gabor_filter(illmask, filter)
     BsMat = np.zeros(img.shape)
-    image_global = cv2.copyMakeBorder(img, 3, 3, 3, 3, cv2.BORDER_REFLECT)
+    image_global = cv2.copyMakeBorder(illmask, 3, 3, 3, 3, cv2.BORDER_REFLECT)
     width, height = image_global.shape
     
     for i in range(3, width-3):
@@ -96,17 +96,25 @@ def jnc_plus(img, filter=None, name=None):
                 g2,_ = localneighbors(image_global, i, j, d=3)
                 Bs = (np.sum(g2)-np.sum(g1))/24
                 BsMat[i-3, j-3] = gabor_img[i-3, j-3]/Bs
-                # print(gabor_img[i-3, j-3], Bs)
-
+    
     # normalize the image
-    BMat = BsMat/np.max(BsMat)
+    BsMat_normalize = BsMat/np.max(BsMat)
+    
     # save the image to a file in the folder
+    BMat = np.where(BsMat > np.mean(BsMat), 1, 0)
     # get the folder link
     folder_link = "/home/nguyentansy/DATA/PhD-work/PhD-project/2021/src/Pre-processing/Isotropic/UnevenIllumination/src/img_ppt/alc"
     # save the image to a file in the folder
-    cv2.imwrite(folder_link+"/jnc_plus_"+name+".png", BMat*255)
+    plt.imsave(folder_link+"/jnc_plus_"+name+".png", BMat, cmap='gray', vmin=0, vmax=1)
     # save gambor image to a file in the folder
     cv2.imwrite(folder_link+"/gabor_"+name+".png", gabor_img)
+    
+    result = plt.hist(BsMat_normalize.ravel(), color='c', edgecolor='k', alpha=0.65)
+    plt.axvline(BsMat_normalize.mean(), color='k', linestyle='dashed', linewidth=1)
+    min_ylim, max_ylim = plt.ylim()
+    plt.text(BsMat_normalize.mean()*1.1, max_ylim*0.9, 'Mean: {:.2f}'.format(BsMat_normalize.mean()))
+    plt.savefig(folder_link+"/fig_jnc_plus_"+name+".png")
+
     return np.mean(BsMat)
 
 
@@ -130,7 +138,7 @@ def jnc(img, d=1, name=None):
     BsMat = np.zeros(img.shape)
     res = np.zeros(img.shape)
     # BgMat = np.zeros(img.shape)
-    image = cv2.copyMakeBorder(img, 1, 1, 1, 1, cv2.BORDER_REFLECT)
+    image = cv2.copyMakeBorder(illmask, 1, 1, 1, 1, cv2.BORDER_REFLECT)
     # image_global = cv2.copyMakeBorder(illmask, d, d, d, d, cv2.BORDER_REFLECT)
     width, height = image.shape
     Bg = np.mean(illmask)
@@ -149,8 +157,10 @@ def jnc(img, d=1, name=None):
                 B_top = np.abs(g.astype(float)-float(Ba))
                 BsMat[i-1, j-1] = np.max(B_top/Ba)
 
-        # normalize the image
-    BMat = BsMat/np.max(BsMat)
+    # normalize the image
+    # BMat = BsMat/np.max(BsMat)
+    # binarize the image with a threshold of 0.5
+    BMat = np.where(BsMat > np.mean(BsMat), 1, 0)
     # save the image to a file in the folder
     # get the folder link
     folder_link = "/home/nguyentansy/DATA/PhD-work/PhD-project/2021/src/Pre-processing/Isotropic/UnevenIllumination/src/img_ppt/alc"
@@ -179,8 +189,8 @@ if __name__ == '__main__':
         img = cv2.imread(file)
         img2 = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         jnc_plus_level = jnc_plus(img[:, :, 2], filter=filter, name = name)
-        jnc_level = jnc(img[:, :, 2], name = name)
-        # print(jnc_level)
+        # jnc_level = jnc(img[:, :, 2], name = name)
+        print(jnc_plus_level)
 
     # with open('test_artificial.csv', 'w') as f:
     #     writer = csv.writer(f, delimiter='\t')
