f44cd4e2cb38cca82cc62f3ccd3a1ad19a475bd7
tansyab1 <tansyab1@gmail.com>
2022-12-15 19:50:03 +0100

    test

--- 2021/src/Classification/kvasir-capsule/classification_experiments/Modified-ResNet-152/improvedTripletPredictor.py
+++ 2021/src/Classification/kvasir-capsule/classification_experiments/Modified-ResNet-152/improvedTripletPredictor.py
@@ -759,38 +759,15 @@ def plot_confusion_matrix(cm, classes,
 
 
 # ==============================================
-# function to windown partitioning
+# function to windown partitioning the features map in to different patches
 # ==============================================
 
-def window_partition(x, win_size, dilation_rate=1):
-    B, H, W, C = x.shape
-    if dilation_rate != 1:
-        x = x.permute(0, 3, 1, 2)  # B, C, H, W
-        assert type(dilation_rate) is int, 'dilation_rate should be a int'
-        x = F.unfold(x, kernel_size=win_size, dilation=dilation_rate, padding=4 *
-                     (dilation_rate-1), stride=win_size)  # B, C*Wh*Ww, H/Wh*W/Ww
-        windows = x.permute(0, 2, 1).contiguous().view(-1,
-                                                       C, win_size, win_size)  # B' ,C ,Wh ,Ww
-        windows = windows.permute(0, 2, 3, 1).contiguous()  # B' ,Wh ,Ww ,C
-    else:
-        x = x.view(B, H // win_size, win_size, W // win_size, win_size, C)
-        windows = x.permute(0, 1, 3, 2, 4, 5).contiguous(
-        ).view(-1, win_size, win_size, C)  # B' ,Wh ,Ww ,C
-    return windows
+def window_partition(x, window_size):
+    # divide the image into patches with size of window_size * window_size
 
-
-def window_reverse(windows, win_size, H, W, dilation_rate=1):
-    # B' ,Wh ,Ww ,C
-    B = int(windows.shape[0] / (H * W / win_size / win_size))
-    x = windows.view(B, H // win_size, W // win_size, win_size, win_size, -1)
-    if dilation_rate != 1:
-        # B, C*Wh*Ww, H/Wh*W/Ww
-        x = windows.permute(0, 5, 3, 4, 1, 2).contiguous()
-        x = F.fold(x, (H, W), kernel_size=win_size, dilation=dilation_rate,
-                   padding=4*(dilation_rate-1), stride=win_size)
-    else:
-        x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
-    return x
+    B, C, H, W = x.size()
+    x = x.view(B, C, H // window_size, window_size,
+               W // window_size, window_size)
 
 
 # ========================================
