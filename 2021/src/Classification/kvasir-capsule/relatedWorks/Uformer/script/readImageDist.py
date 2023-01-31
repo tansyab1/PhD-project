f8171a398a4ecdad2e57d3eaa4412b29456c6d5d
tansyab1 <tansyab1@gmail.com>
2023-01-31 13:07:56 +0100

    update

--- 2021/src/Classification/kvasir-capsule/relatedWorks/Uformer/script/readImageDist.py
+++ 2021/src/Classification/kvasir-capsule/relatedWorks/Uformer/script/readImageDist.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import tqdm
+import glob
 
 # define a function to read the image from the foler and find the reference image
 
@@ -16,22 +17,69 @@ def readImageDist(distPath, refPath, savePath):
             # get the image name
             name = file.split('.')[0]
 
-            #  find the reference image refPath with the same name and save it to the savePath
+            #  find the reference image refPath subfolder with the same name and save it to the savePath
 
             for root, dirs, files in os.walk(refPath):
                 for file in files:
+                    # print(file)
                     if file == name + '.jpg':
-                        # get the reference image path
-                        refImage = os.path.join(refPath, file)
+                        # get the reference image path from the refPath
+                        refImage = os.path.join(root, dirs)
+                        refImage = os.path.join(refImage, file)
+                        
+                        
+                        
                         # get the save image path
                         saveImage = os.path.join(savePath, name + '.jpg')
                         # copy the reference image to the savePath
                         os.system('cp ' + refImage + ' ' + saveImage)
+                        break
+                    break
                         
 if __name__ == "__main__":
     # define the path of the distorted image and the reference image
-    refPath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/distorted_images/Blur_var'
+    refPath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/Ref'
     distPath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/Blur_var/train'
     # define the path to save the reference image
     savePath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/Blur_var/train_groundtruth'
+    readImageDist(distPath, refPath, savePath)
+    
+    distPath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/Blur_var/val'
+    savePath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/Blur_var/val_groundtruth'
+    
+    readImageDist(distPath, refPath, savePath)
+    
+    distPath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/Blur_var/test'
+    savePath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/Blur_var/test_groundtruth'
+    
+    readImageDist(distPath, refPath, savePath)
+    
+    distPath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/Noise_var/train'
+    savePath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/Noise_var/train_groundtruth'
+    
+    readImageDist(distPath, refPath, savePath)
+    
+    distPath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/Noise_var/val'
+    savePath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/Noise_var/val_groundtruth'
+    
+    readImageDist(distPath, refPath, savePath)
+    
+    distPath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/Noise_var/test'
+    savePath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/Noise_var/test_groundtruth'
+    
+    readImageDist(distPath, refPath, savePath)
+    
+    distPath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/UI_var/train'
+    savePath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/UI_var/train_groundtruth'
+    
+    readImageDist(distPath, refPath, savePath)
+    
+    distPath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/UI_var/val'
+    savePath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/UI_var/val_groundtruth'
+    
+    readImageDist(distPath, refPath, savePath)
+    
+    distPath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/UI_var/test'
+    savePath = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_images/process/labelled_images/ExperimentalDATA/forRelatedWorks/UI_var/test_groundtruth'
+    
     readImageDist(distPath, refPath, savePath)
