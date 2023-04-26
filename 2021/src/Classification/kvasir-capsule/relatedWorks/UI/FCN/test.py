40cee7b6cfa00610c3161956531b5c9c83afccee
tansyab1 <tansyab1@gmail.com>
2023-04-26 18:15:14 +0200

    update

--- 2021/src/Classification/kvasir-capsule/relatedWorks/UI/FCN/test.py
+++ 2021/src/Classification/kvasir-capsule/relatedWorks/UI/FCN/test.py
@@ -6,7 +6,7 @@
 ## ECCV 2020
 ## https://arxiv.org/abs/2003.06792
 """
-
+from collections import OrderedDict
 
 import numpy as np
 import os
@@ -24,16 +24,20 @@ from psnr import batch_PSNR, batch_SSIM, save_img
 from dataloader import get_validation_data
 from skimage import img_as_ubyte
 
-parser = argparse.ArgumentParser(description='RGB deblurring evaluation on the validation set of Noisevar')
+parser = argparse.ArgumentParser(
+    description='RGB deblurring evaluation on the validation set of Noisevar')
 parser.add_argument('--input_dir', default='./dataset/UI_var/test/',
-    type=str, help='Directory of validation images')
+                    type=str, help='Directory of validation images')
 parser.add_argument('--result_dir', default='./results/',
-    type=str, help='Directory for results')
+                    type=str, help='Directory for results')
 parser.add_argument('--weights', default='./checkpoints/UIcorrection/models/FCN/model_best.pth',
-    type=str, help='Path to weights')
-parser.add_argument('--gpus', default='0', type=str, help='CUDA_VISIBLE_DEVICES')
-parser.add_argument('--bs', default=16, type=int, help='Batch size for dataloader')
-parser.add_argument('--save_images', action='store_true', help='Save denoised images in result directory')
+                    type=str, help='Path to weights')
+parser.add_argument('--gpus', default='0', type=str,
+                    help='CUDA_VISIBLE_DEVICES')
+parser.add_argument('--bs', default=16, type=int,
+                    help='Batch size for dataloader')
+parser.add_argument('--save_images', action='store_true',
+                    help='Save denoised images in result directory')
 
 args = parser.parse_args()
 
@@ -44,19 +48,32 @@ os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
 os.makedirs(args.result_dir, exist_ok=True)
 
 test_dataset = get_validation_data(args.input_dir)
-test_loader = DataLoader(dataset=test_dataset, batch_size=args.bs, shuffle=False, num_workers=8, drop_last=False)
+test_loader = DataLoader(dataset=test_dataset, batch_size=args.bs,
+                         shuffle=False, num_workers=8, drop_last=False)
 
+
+def load_checkpoint(model, weights):
+    checkpoint = torch.load(weights)
+    try:
+        model.load_state_dict(checkpoint["state_dict"])
+    except:
+        state_dict = checkpoint["state_dict"]
+        new_state_dict = OrderedDict()
+        for k, v in state_dict.items():
+            name = k[7:]  # remove `module.`
+            new_state_dict[name] = v
+        model.load_state_dict(new_state_dict)
 
 
 model_restoration = FCN()
 
 # load weights
-utils.load_checkpoint(model_restoration,args.weights)
+load_checkpoint(model_restoration, args.weights)
 print("===>Testing using weights: ", args.weights)
 
 model_restoration.cuda()
 
-model_restoration=nn.DataParallel(model_restoration)
+model_restoration = nn.DataParallel(model_restoration)
 
 model_restoration.eval()
 
@@ -71,8 +88,8 @@ with torch.no_grad():
         filenames = data_test[2]
         rgb_restored = model_restoration(rgb_noisy)
         # print(rgb_restored.shape)
-        rgb_restored = torch.clamp(rgb_restored,0,1)
-     
+        rgb_restored = torch.clamp(rgb_restored, 0, 1)
+
         psnr_val_rgb.append(batch_PSNR(rgb_restored, rgb_gt, 1.))
         ssim_val_rgb.append(batch_SSIM(rgb_restored, rgb_gt, 1.))
 
@@ -83,10 +100,10 @@ with torch.no_grad():
         if args.save_images:
             for batch in range(len(rgb_gt)):
                 denoised_img = img_as_ubyte(rgb_restored[batch])
-                save_img(args.result_dir + filenames[batch][:-4] + '.png', denoised_img)
-            
+                save_img(args.result_dir +
+                         filenames[batch][:-4] + '.png', denoised_img)
+
 psnr_val_rgb = sum(psnr_val_rgb)/len(psnr_val_rgb)
 ssim_val_rgb = sum(ssim_val_rgb)/len(ssim_val_rgb)
-print("PSNR: %.2f " %(psnr_val_rgb))
-print("SSIM: %.4f " %(ssim_val_rgb))
-
+print("PSNR: %.2f " % (psnr_val_rgb))
+print("SSIM: %.4f " % (ssim_val_rgb))
