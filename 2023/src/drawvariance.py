06d6d52389bc2c2782b3a55d382b42f18f645679
tansyab1 <tansyab1@gmail.com>
2023-05-19 23:49:32 +0200

    update

--- 2023/src/drawvariance.py
+++ 2023/src/drawvariance.py
@@ -30,12 +30,13 @@ for image in os.listdir(pathOut):
         img = img.convert('L')
         # convert image to tensor
         img = transforms.ToTensor()(img)
+        print(np.mean(img.numpy()))
         # calculate variance and entropy of image
-        variance = np.var(img.numpy())
-        entropy = -np.sum(img.numpy() * np.log2(img.numpy() + 1e-10))
+        variance = np.std(img.numpy())
+        # entropy = -np.sum(img.numpy() * np.log2(img.numpy() + 1e-10))
         # add variance and entropy to list
         variance_list.append(variance)
-        entropy_list.append(entropy)
+        # entropy_list.append(entropy)
 
 # plot histogram of variance and entropy with mininumum border
 plt.hist(variance_list, color='blue', edgecolor='black')
@@ -43,7 +44,7 @@ plt.hist(variance_list, color='blue', edgecolor='black')
 plt.savefig('/home/nguyentansy/DATA/PhD-work/PhD-project/2023/src/variance.png'
             )
 
-plt.hist(entropy_list, color='red', edgecolor='black')
-# save histogram to file .eps
-plt.savefig('/home/nguyentansy/DATA/PhD-work/PhD-project/2023/src/entropy.png'
-            )
+# plt.hist(entropy_list, color='red', edgecolor='black')
+# # save histogram to file .eps
+# plt.savefig('/home/nguyentansy/DATA/PhD-work/PhD-project/2023/src/entropy.png'
+#             )
