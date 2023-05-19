e1d60448eb8f47c605b5c1a1e80de53772c59db3
tansyab1 <tansyab1@gmail.com>
2023-05-19 22:05:19 +0200

    update

---
+++ 2023/src/tsne.py
@@ -0,0 +1,96 @@
+from PIL import Image
+from torch.autograd import Variable
+import torchvision.transforms as transforms
+import torchvision.models as models
+import torch
+import numpy as np
+import os
+import cv2
+# import PCA library
+from sklearn.decomposition import PCA
+# iport TSNE library
+from sklearn.manifold import TSNE
+# import matplotlib library
+import matplotlib.pyplot as plt
+
+pathOut = '/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/labelled_videos/\
+    process/labelled_videos_process/main/KvasirCapsuleIQA/final/imgs/'
+# for video in os.listdir(pathIn):
+#     if video.endswith(".mp4"):
+#         extractImages(pathIn + video, pathOut)
+
+
+# load the model VGG16 pretrained on ImageNet
+vgg16 = models.vgg16(pretrained=True)
+# remove the last layer
+vgg16.classifier = torch.nn.Sequential(*list(vgg16.classifier.children())[:-1])
+# set model to evaluation mode
+vgg16.eval()
+# move model to cuda
+vgg16.cuda()
+pca = PCA(n_components=4096)
+tsne = TSNE(n_components=2, random_state=0)
+
+# extract features from all images from image folder
+
+
+def extractFeature(pathIn):
+    # get image name
+    image_name = pathIn.split('/')[-1].split('.')[0]
+    # load image
+    img = Image.open(pathIn)
+    # resize image to 224x224
+    img = img.resize((224, 224))
+    # convert image to tensor
+    img = transforms.ToTensor()(img)
+    # normalize image
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225]
+                                     )
+    img = normalize(img)
+    # add dimension to image
+    img = img.unsqueeze(0)
+    # move image to cuda
+    img = Variable(img).cuda()
+    # extract feature from image
+    feature = vgg16(img)
+    # convert feature to numpy array
+    feature = feature.cpu().data.numpy()
+    # extract feature and reduce to 4096 dimension using using PCA
+    feature = pca.fit_transform(feature)
+    # add all features to a list and return
+    return feature, image_name
+
+# function to extract features from all images in folder and use TSNE for visualization
+# Path: 2023/src/extractFeature.py
+
+
+def TSNEvisualiza(inpath):
+    # create a list to store all features
+    features = []
+    # create a list to store all image names
+    img_names = []
+    # read all images in folder
+    for img in os.listdir(inpath):
+        # extract feature from image
+        feature, name = extractFeature(inpath + img)
+        # add feature to list
+        features.append(feature)
+        # add image name to list
+        img_names.append(name)
+    # convert list to numpy array
+    features = np.array(features)
+    # reshape array to 2D
+    features = features.reshape(features.shape[0], features.shape[2])
+    # use TSNE to reduce dimension to 2D
+    features = tsne.fit_transform(features)
+    # plot the result
+    plt.figure(figsize=(10, 10))
+    plt.scatter(features[:, 0], features[:, 1], c='b', marker='x')
+    # for i, txt in enumerate(img_names):
+    #     plt.annotate(txt, (features[i, 0], features[i, 1]))
+    plt.show()
+
+
+if __name__ == '__main__':
+    TSNEvisualiza(pathOut)
