bd2b04a273ee98d3cb5bd8370fbada30c4ed7a6b
tansyab1 <tansyab1@gmail.com>
2023-05-20 11:17:42 +0200

    update

--- 2023/src/loaddb.sql
+++ 2023/src/loaddb.sql
@@ -1,3 +1,3 @@
--- each table in the database and save it to a .csv file
-SELECT * FROM `1` INTO OUTFILE './1.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
-SELECT * FROM `2` INTO OUTFILE './2.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
+-- select all elements of the COLUMN 'birthdate' in table 'userNote'
+SELECT birthdate FROM userNote;
+```
