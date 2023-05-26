00f897f155aab255eafbd119a0b24fc3b862c5eb
tansyab1 <tansyab1@gmail.com>
2023-05-26 07:50:14 +0200

    update

--- tansy.nguyen@gaia.math.univ-paris13.fr/utils/CheckShapeModel.py
+++
@@ -1,9 +0,0 @@
-#  file to check shape of the model
-import torch
-import os 
-import torchvision.models as models
-
-
-if __name__ == '__main__':
-    model = models.resnet152(pretrained=True)
-    print(model)
