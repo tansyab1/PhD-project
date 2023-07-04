f81fac6abd80d73fce6c7be3aa4ef2fa3bb56319
tansyab1 <tansyab1@gmail.com>
2023-07-04 09:52:40 +0200

    update

--- 2023/Classification/WindowDilatedAttention-SSL/scripts/generate_image_from_video.py
+++ 2023/Classification/WindowDilatedAttention-SSL/scripts/generate_image_from_video.py
@@ -2,24 +2,21 @@ import glob
 import os
 import argparse
 import cv2
-# import tqdm as tqdm
+from tqdm import tqdm as tqdm
 
 parser = argparse.ArgumentParser(
     description="Generate the image from the video.")
-parser.add_argument("-d", "--data_dir", type=str)
-parser.add_argument("-o", "--output_dir", type=str)
+parser.add_argument("-d", "--data_dir", type=str, default="/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/unlabelled_videos")
+parser.add_argument("-o", "--output_dir", type=str, default="/home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/unlabelled_videos_images")
 
 
 def extract_img(data_dir, dest_dir):
     frame_count = 0
     data_dir = sorted(list(glob.glob("%s/*" % data_dir)),
                       key=lambda x: x.split("/")[-2])
-    for video in data_dir:
+    for video in tqdm(data_dir):
         video_name = os.path.basename(video)
         video_name = video_name.split(".")[0]
-        dest_dir = dest_dir + "/" + video_name
-        if not os.path.exists(dest_dir):
-            os.makedirs(dest_dir)
         cap = cv2.VideoCapture(video)
         frame_count = 0
         while True:
@@ -27,7 +24,7 @@ def extract_img(data_dir, dest_dir):
             if ret is False:
                 break
             cv2.imwrite(os.path.join(
-                dest_dir, str(frame_count) + ".jpg"), frame)
+                dest_dir, video_name + "_%d.jpg" % frame_count), frame)
             frame_count += 1
         cap.release()
 
