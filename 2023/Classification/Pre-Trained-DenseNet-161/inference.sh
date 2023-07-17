cf9750e262f394adb43df3ef912b628dc82a310c
tansyab1 <tansyab1@gmail.com>
2023-07-17 14:59:10 +0200

    update

--- 2023/Classification/Pre-Trained-DenseNet-161/inference.sh
+++ 2023/Classification/Pre-Trained-DenseNet-161/inference.sh
@@ -3,10 +3,10 @@
 
 python3 densenet161_split_0.py \
     --data_root /home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/unlabelled_images \
-    --data_to_inference /home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/unlabelled_images_folds/8 \
+    --data_to_inference /home/nguyentansy/DATA/PhD-work/Datasets/kvasir_capsule/unlabelled_images_folds/9 \
     --out_dir ./result/output \
     --tensorboard_dir ./result/tensorboard \
     --bs 1 --num_epochs 1 \
     --action inference \
-    --val_fold "8" 
+    --val_fold "9" 
 
