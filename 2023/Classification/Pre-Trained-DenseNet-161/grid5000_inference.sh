5653768603561c1a56be068581048316c6ca958c
tansyab1 <tansyab1@gmail.com>
2023-07-18 09:41:57 +0200

    update

---
+++ 2023/Classification/Pre-Trained-DenseNet-161/grid5000_inference.sh
@@ -0,0 +1,12 @@
+# SH file for inference the DenseNet-161 model "./result/output/densenet161_split_0.py/checkpoints/densenet161_split_0.py_epoch:22.pt"
+
+
+python3 densenet161_split_0.py \
+    --data_root ./2 \
+    --data_to_inference ./2 \
+    --out_dir ./result/output \
+    --tensorboard_dir ./result/tensorboard \
+    --bs 1 --num_epochs 1 \
+    --action inference \
+    --val_fold "2" 
+
